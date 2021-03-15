38934160e3a2183a95609f94501da52faef58be8
lurui <1332395618@qq.com>
2021-03-15 15:59:02 +0800

    修改无需的模块

--- spug_web/src/config/menu.js
+++ spug_web/src/config/menu.js
@@ -21,28 +21,13 @@ let menu = {
         {
             key: '/publish/image', desc: '镜像管理', icon: 'fa fa-flag-o', permission: 'publish_app_view|publish_image_view',
         },
-        // {
-        //     key: '4', desc: '配置管理', icon: 'fa fa-cogs', permission: 'config_environment_view|config_service_view|config_app_view', subs: [
-        //         {key: '/configuration/environment', permission: 'config_environment_view', desc: '环境管理'},
-        //         {key: '/configuration/service', permission: 'config_service_view', desc: '服务配置'},
-        //         {key: '/configuration/app', permission: 'config_app_view', desc: '应用配置'},
-        //         {key: '/configuration/relationship', permission: 'config_app_view', desc: '配置关系'}
-        //     ]
-        // },
-
         {
-            key: '5', desc: '任务管理', icon: 'fa fa-calendar', permission: 'job_task_view', subs: [
+            key: '3', desc: '任务管理', icon: 'fa fa-calendar', permission: 'job_task_view', subs: [
                 {key: '/schedule/job', permission: 'job_task_view', desc: '任务列表'}
             ]
         },
-        // {
-        //     key: '7', desc: '告警管理', icon: 'fa fa-bell-o', subs: [
-        //         {key: 'alarm_role', desc: '报警规则'},
-        //         {key: 'alarm_contact', desc: '报警联系人'},
-        //     ]
-        // },
         {
-            key: '8', desc: '系统管理', icon: 'el-icon-setting',  permission: 'system_notify_view',  subs: [
+            key: '4', desc: '系统管理', icon: 'el-icon-setting',  permission: 'system_notify_view',  subs: [
                 {key: '/system/notify', permission: 'system_notify_view', desc: '通知设置'},
             ]
         },
