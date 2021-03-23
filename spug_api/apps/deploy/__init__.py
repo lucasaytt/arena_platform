f7dfa2102e27bdf9382bef9369787c1218418dc1
lurui <1332395618@qq.com>
2021-03-23 20:43:11 +0800

    去除多余代码

--- spug_api/apps/deploy/__init__.py
+++ spug_api/apps/deploy/__init__.py
@@ -1,10 +1,5 @@
-
 from apps.deploy import image
 
-
-
 def register_blueprint(app):
-
     app.register_blueprint(image.blueprint, url_prefix='/deploy/images')
-    app.register_blueprint(exec.blueprint, url_prefix='/deploy/exec')
 
