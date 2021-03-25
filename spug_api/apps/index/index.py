fbfa6072a4b7f09de543c5d4533bb1fc0fd2e585
lurui <1332395618@qq.com>
2021-03-25 20:07:05 +0800

    添加接入页面

--- spug_api/apps/index/index.py
+++ spug_api/apps/index/index.py
@@ -5,12 +5,7 @@ from flask import Blueprint
 
 blueprint = Blueprint(__name__, __name__)
 
-
 @blueprint.route('/', methods=['GET'])
-def index():
-    return app.send_static_file('index.html')
-
-
 @blueprint.route('', methods=['GET'])
-def get():
+def index():
     return app.send_static_file('index.html')
