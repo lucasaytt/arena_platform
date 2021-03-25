0bcc8f89ff9cdd0ce65caf7367b2e1236c2079c1
lurui <1332395618@qq.com>
2021-03-25 19:41:11 +0800

    添加接入页面

--- spug_api/apps/index/index.py
+++ spug_api/apps/index/index.py
@@ -9,3 +9,8 @@ blueprint = Blueprint(__name__, __name__)
 @blueprint.route('/', methods=['GET'])
 def index():
     return app.send_static_file('index.html')
+
+
+@blueprint.route('', methods=['GET'])
+def index():
+    return app.send_static_file('index.html')
