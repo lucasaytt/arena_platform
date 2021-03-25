bf820ff66153c5e756bfe79b92c64142eaf465cd
lurui <1332395618@qq.com>
2021-03-25 19:47:30 +0800

    添加接入页面

--- spug_api/apps/index/__init__.py
+++ spug_api/apps/index/__init__.py
@@ -3,4 +3,3 @@ from apps.index import index
 
 def register_blueprint(app):
     app.register_blueprint(index.blueprint, url_prefix='/index')
-    app.register_blueprint(index.blueprint, url_prefix='/index/')
