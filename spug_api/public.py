5b198418efa494c2f8154f45c2763819aae99d19
lurui <1332395618@qq.com>
2021-03-30 13:23:23 +0800

    添加url拦截

--- spug_api/public.py
+++ spug_api/public.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import config
+from flask_login import login_manager
 
 
 app = Flask(__name__, static_url_path='')
+login_manager.LoginManager.init_app(app)
 app.config.from_object(config)
 db = SQLAlchemy(app)
