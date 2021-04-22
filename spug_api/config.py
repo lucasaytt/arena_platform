b866729d32c749e487566232e7c003d10efcf9db
lucasaytt <42641861+lucasaytt@users.noreply.github.com>
2021-04-22 15:41:27 +0800

    Update config.py

--- spug_api/config.py
+++ spug_api/config.py
@@ -4,7 +4,7 @@ import os
 DEBUG = True
 TIME_ZONE = timezone('Asia/Shanghai')
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://hive_user:J2q1mbVBRMSHvucC@cn-bdp-mysql01.corp.cootek.com/tensorflow'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://xxxxx:xxxxx@cn-bdp-mysql01.corp.cootek.com/tensorflow'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = False
 
